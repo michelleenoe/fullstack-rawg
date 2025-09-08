6578395ab3b5727e1a8735464d7495eb4d13fc78
michelleenoe <122521774+michelleenoe@users.noreply.github.com>
2025-09-08 10:15:21 +0200

    base layout removed css

--- rawg-dummy/frontend/src/main.tsx
+++ rawg-dummy/frontend/src/main.tsx
@@ -1,13 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { ChakraProvider } from '@chakra-ui/react'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <ChakraProvider>
-    <App />
+import { ChakraProvider, ColorModeScript, extendTheme, type ThemeConfig } from '@chakra-ui/react';
+
+const config: ThemeConfig = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
+};
+const theme = extendTheme({ config });
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <App />
     </ChakraProvider>
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
