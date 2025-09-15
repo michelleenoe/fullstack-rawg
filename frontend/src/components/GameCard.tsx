89d4cdaeaaa1e67fc7fdcf2f558a10f658d323c5
Michelle Enøe <mien0001@stud.ek.dk>
2025-09-15 10:33:29 +0200

    Initial commit

---
+++ frontend/src/components/GameCard.tsx
@@ -0,0 +1,26 @@
+import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
+
+import type { Game } from "../lib/hooks/useGames";
+import CriticScore from "./CriticScore";
+import PlatformIconsList from "./PlatformIconsList";
+
+interface Props {
+  game: Game;
+}
+
+export const GameCard = ({ game }: Props) => {
+  return (
+    <Card borderRadius={10} overflow="hidden">
+      <Image src={game.background_image} alt={game.name} />
+      <HStack justifyContent="space-between" padding={2}>
+        <PlatformIconsList
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        />
+        <CriticScore score={game.metacritic} />
+      </HStack>
+      <CardBody>
+        <Heading fontSize="2xl">{game.name}</Heading>
+      </CardBody>
+    </Card>
+  );
+};
---
+++ frontend/src/components/GameGrid.tsx
@@ -0,0 +1,22 @@
+import { SimpleGrid, Text } from "@chakra-ui/react";
+import useGames from "../lib/hooks/useGames";
+import { GameCard } from "./GameCard";
+
+export const GameGrid = () => {
+  const { games, error } = useGames();
+
+  return (
+    <div>
+      {error && <Text color="red.500">{error}</Text>}
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        spacing={4}
+        padding={2}
+      >
+        {games.map((game) => (
+          <GameCard key={game.id} game={game} />
+        ))}
+      </SimpleGrid>
+    </div>
+  );
+};
